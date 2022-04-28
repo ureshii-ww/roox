5dec0fbba6ecbe2ff470a5e0a43b4c30376dc308
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-28 22:31:13 +0700

    Added InputTextarea component

---
+++ src/components/UI/InputTextarea/InputTextarea.tsx
@@ -0,0 +1,14 @@
+import React, { forwardRef, TextareaHTMLAttributes } from 'react';
+
+type InputProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const InputTextarea = forwardRef<HTMLTextAreaElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    const baseClass = 'input-textarea';
+    const classString = className ? `${className} ${baseClass}` : baseClass;
+
+    return <textarea {...props} ref={ref} className={classString} />;
+  }
+);
+
+export default InputTextarea;
