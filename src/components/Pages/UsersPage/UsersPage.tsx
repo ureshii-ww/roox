392bcb558eb514c8711aabf3b22243a562bf380c
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-30 22:31:02 +0700

    UsersPage styling

--- src/components/Pages/UsersPage/UsersPage.tsx
+++ src/components/Pages/UsersPage/UsersPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import UsersList from '../../Containers/UsersList/UsersList';
+import './users-page.scss';
 
 const UsersPage = () => {
   return (
-    <div>
+    <div className="users-page">
+      <h1 className="users-page__title">Список пользователей</h1>
       <UsersList />
     </div>
   );
