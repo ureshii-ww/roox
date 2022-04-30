1c7acdb3dc520b94762328eada5a407625a76a05
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-30 22:14:22 +0700

    Button styling

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React, { ButtonHTMLAttributes, FC } from 'react';
+import './button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: FC<ButtonProps> = ({ className, children, ...props }) => {
   const baseClass = 'main-button';
   const classString = className ? `${className} ${baseClass}` : baseClass;
-  
+
   return (
     <button {...props} className={classString}>
       {children}
