a9e0d2d0a52a8ee0350d71366bac60dd484492ee
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-30 22:21:47 +0700

    Sidebar styling

--- src/components/Containers/Sidebar/Sidebar.tsx
+++ src/components/Containers/Sidebar/Sidebar.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Button from '../../UI/Button/Button';
 import { useSidebar } from './useSidebar';
+import './sidebar.scss';
 
 const Sidebar = () => {
-  const {handleSortByName, handleSortByCity, isSortedByCity, isSortedByName} = useSidebar();
-  
+  const { handleSortByName, handleSortByCity, isSortedByCity, isSortedByName } = useSidebar();
+
   return (
-    <div>
-      <div>
-        <h2>Сортировка</h2>
-        <Button onClick={handleSortByCity} disabled={isSortedByCity}>По городу</Button>
-        <Button onClick={handleSortByName} disabled={isSortedByName}>По имени</Button>
+    <div className="sidebar">
+      <div className="sidebar__controls">
+        <h2 className="sidebar__title">Сортировка</h2>
+        <Button className="sidebar__button" onClick={handleSortByCity} disabled={isSortedByCity}>
+          По городу
+        </Button>
+        <Button className="sidebar__button" onClick={handleSortByName} disabled={isSortedByName}>
+          По имени
+        </Button>
       </div>
     </div>
   );
