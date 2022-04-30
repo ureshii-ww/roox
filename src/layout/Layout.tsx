5c1c8ba69c8bbc572c3a28b355624ca30fe347cc
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-30 23:48:42 +0700

    Layout styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import AppRouter from '../routes/AppRouter';
 import Sidebar from '../components/Containers/Sidebar/Sidebar';
+import './layout.scss';
 
 const Layout = () => {
   return (
-    <div>
-      <Sidebar />
-      <AppRouter />
+    <div className="app-layout">
+      <div className="app-layout__content">
+        <Sidebar />
+        <div className="app-layout__page">
+          <AppRouter />
+        </div>
+      </div>
     </div>
   );
 };
